4b88917327cbfbc347217e24812083f3caf1c9a5
David Adekoya <davidadekoya88@gmail.com>
2026-05-06 14:11:34 +0100

    ddjj

---
+++ lib/presentation/screens/tabs/add_customer.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:formz/formz.dart';
+import 'package:gasly/constants/app_colors.dart';
+import 'package:gasly/constants/app_spacing.dart';
+
+
+class AddCustomerScreen extends HookWidget {
+  const AddCustomerScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+
+  return BlocConsumer<CustomerBloc, CustomerState>(
+
+listener: (context, state) {
+  
+},
+
+
+  )
+
+
+
+  }
+}
